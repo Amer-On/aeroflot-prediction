661039c71196b1e15a83309bc2d29c027034450d
Amer-On <a20051772@yandex.ru>
2023-05-19 23:55:58 +0300

    python and pip installation and setup

--- server_setup.sh
+++ server_setup.sh
@@ -1,11 +1,26 @@
 #!/bin/bash
 # update apt
-sudo apt update && sudo apt upgrade
-# install 
+yes | sudo apt update && sudo apt upgrade
+yes | sudo apt install software-properties-common -y
+# INSTALL PYTHON
 sudo apt install wget build-essential libncursesw5-dev libssl-dev \
 libsqlite3-dev tk-dev libgdbm-dev libc6-dev libbz2-dev libffi-dev zlib1g-dev
-sudo add-apt-repository ppa:deadsnakes/ppa
-sudo apt install python3.11
+yes | sudo add-apt-repository ppa:deadsnakes/ppa
+yes | sudo apt install python3.11
 echo alias py=python3.11 >> ~/.bashrc
 echo alias python=python3.11 >> ~/.bashrc
-# then run ~/.bashrc or restart session
+echo alias python3=python3.11 >> ~/.bashrc
+yes | sudo apt install python3.11-venv
+python3.11 -m ensurepip
+echo alias pip=pip3 >> ~/.bashrc
+yes | python3.11 -m pip install --upgrade pip
+# remove previos python version if needed
+# check installed packages
+# apt list --installed
+# sudo apt-get remove python3.5
+# remove python with dependencies and data files
+# sudo apt-get purge --auto-remove python3.5
+# run ~/.bashrc to apply aliases
+exec bash
+source ~/.bashrc
+exec zsh
