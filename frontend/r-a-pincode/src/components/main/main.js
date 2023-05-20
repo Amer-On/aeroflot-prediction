3b48b00bbfbae39860aceb3116be40163704508d
xXseeker <mrhohiha@gmail.com>
2023-05-20 23:29:09 +0300

    Структура папок. Начало реакт приложения

---
+++ frontend/r-a-pincode/src/components/main/main.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Main () {
+    return(
+        <div className="main-content">
+            <h1>Hello world!</h1>
+        </div>
+    );
+}
+
+export default Main;
