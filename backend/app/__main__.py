ca114cbac9cd025d59232d273067b520b0f7a4fa
Amer-On <a20051772@yandex.ru>
2023-05-25 01:59:55 +0300

    moved scripts to app folder

--- backend/app/__main__.py
+++ backend/app/__main__.py
@@ -5,5 +5,9 @@ import logging
 logging.basicConfig(filename='backend.log', level=logging.DEBUG)
 
 
-if __name__ == "__main__":
+def main():
     uvicorn.run("app:app", host='0.0.0.0', port=8765, reload=True, log_level='info', workers=os.cpu_count())
+
+
+if __name__ == "__main__":
+    main()
