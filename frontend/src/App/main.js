1d80c4a4b048dccbfca38c11e5c2e4cf6559ef3d
xXseeker <mrhohiha@gmail.com>
2023-05-25 02:49:49 +0300

    верстка v0.3

--- frontend/src/App/main.js
+++ frontend/src/App/main.js
@@ -31,7 +31,32 @@ function Main(){
                         </button>
                     </div>
                 </div>
-                <div className='input'></div>
+                <div className='input'>
+                    <form className='main-form'>
+                        <p className='f-form'>
+                            <select >
+                                <option disable>-- Выбирите направление --</option>
+                                <option value="Moscow - Sochi">Москва - Сочи</option>
+                                <option value="Sochi - Moscow">Сочи - Москва</option>
+                            </select>
+                        </p>
+                        <p className='s-form'>
+                            <select >
+                                <option disable>-- Выбирите номер рейса --</option>
+                                <option value="Moscow - Sochi">Москва - Сочи</option>
+                            </select>
+                        </p>
+                        <p className='t-form'>
+                            <select >
+                                <option disable>-- Выбирите направление --</option>
+                                <option value="Moscow - Sochi">Москва - Сочи</option>
+                            </select>
+                        </p>
+                        <p>
+                            <input type="date" name="calendar" value="2012-06-01" max="2012-06-04" min="2012-05-29"></input>
+                        </p>
+                    </form>
+                </div>
             </div>
             <div className='charts'>
                 <h1>asd</h1>
