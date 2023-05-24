bae87333495587f710ba6d0e3084372167e38c47
Amer-On <a20051772@yandex.ru>
2023-05-25 01:47:40 +0300

    added env

---
+++ backend/config.py
@@ -0,0 +1,14 @@
+from dotenv import load_dotenv
+import jwt
+import os
+
+load_dotenv()
+
+JWT_SECRET = os.getenv('JWT_SECRET')
+JWT_ALGORITHM = os.getenv('JWT_ALGORITHM')
+
+
+_super_user_payload = {
+	'user_id': 69
+}
+JWT_SUPERUSER_TOKEN = jwt.encode(_super_user_payload, JWT_SECRET, algorithm=JWT_ALGORITHM)
