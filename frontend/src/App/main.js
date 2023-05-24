32f48b62ccc27efa46a25698e2131b47fe968df2
xXseeker <mrhohiha@gmail.com>
2023-05-24 21:25:18 +0300

    Верстка v0.1

--- frontend/src/App/main.js
+++ frontend/src/App/main.js
@@ -3,15 +3,29 @@ import './main.css';
 function Main(){
     return(
         <div className="main-conatiner">
-            <div className='container'>
+            <div className='header'>
                 <div className='nav'>
                     <div className='logo'></div>
                     <ul>
-                        <li><a href='#'></a></li>
+                        <li></li>
                         <li></li>
                         <li></li>
                     </ul>
                 </div>
+            </div>
+            <div className='form'>
+                <div className='first-form'>
+
+                </div>
+                <div className='second-form'>
+                    
+                </div>
+                <div className='third-form'>
+                    
+                </div>
+            </div>
+            <div className='charts'>
+                <h1>asd</h1>
             </div>
         </div>
     );
