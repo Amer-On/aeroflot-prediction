f1b0f24105753b984e2d65c0b133814f76c04436
Amer-On <a20051772@yandex.ru>
2023-05-26 18:44:34 +0300

    header updated

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -4,11 +4,15 @@ function Header() {
     return (
         <div className='header'>
             <div className='nav'>
-                <div className='logo'></div>
-                <ul>
-                    <li></li>
-                    <li></li>
-                    <li></li>
+                <div className='logo'>
+                    {/* <img src="components/Aeroflot_logo.png" className='logo'/> */}
+                </div>
+                <ul className='header-navigation'>
+                    <li><button>Домой</button></li>
+                    <li><button>Динамика бронирования</button></li>
+                    <li><button>Предсказания спроса</button></li>
+                    <li><button>Профиль спроса</button></li>
+                    <li><button>Определение сезонности</button></li>
                 </ul>
             </div>
         </div>
