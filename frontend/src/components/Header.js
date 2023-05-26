aeafd519747a1845b78b5fe98e03843582fce288
Amer-On <a20051772@yandex.ru>
2023-05-26 19:22:50 +0300

    links in header to another pages

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -1,18 +1,37 @@
 import React from "react";
+import {Link} from "react-router-dom";
 
 function Header() {
     return (
         <div className='header'>
             <div className='nav'>
-                <div className='logo'>
-                    {/* <img src="components/Aeroflot_logo.png" className='logo'/> */}
-                </div>
+                <div className='logo'/>
                 <ul className='header-navigation'>
-                    <li><button>Домой</button></li>
-                    <li><button>Динамика бронирования</button></li>
-                    <li><button>Предсказания спроса</button></li>
-                    <li><button>Профиль спроса</button></li>
-                    <li><button>Определение сезонности</button></li>
+                    <Link to='/'>
+                        <li>
+                            <button>Домой</button>
+                        </li>
+                    </Link>
+                    <Link to='/booking-dynamic'>
+                        <li>
+                            <button>Динамика бронирования</button>
+                        </li>
+                    </Link>
+                    <Link to='/demand-prediction'>
+                        <li>
+                            <button>Предсказания спроса</button>
+                        </li>
+                    </Link>
+                    <Link to='/demand-profile'>
+                        <li>
+                            <button>Профиль спроса</button>
+                        </li>
+                    </Link>
+                    <Link to='/season-detection'>
+                        <li>
+                            <button>Определение сезонности</button>
+                        </li>
+                    </Link>
                 </ul>
             </div>
         </div>
